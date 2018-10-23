dd7fe04cbc2b4a0ee66e12991294249ae3fa63d7
Pratik Panchal <pratik.sp.1112@gmail.com>
2018-10-24 02:12:31 +0530

    "Mini Project"

--- LotteryTicket.c
+++ LotteryTicket.c
@@ -1,11 +1,20 @@
 #include<stdio.h>
 #include<conio.h>
-int count, loc, choice;
+#include<time.h>
+
+long count;
+int choice;
+long soldTicketCounter;
+
+struct timespec {
+    time_t   tv_sec;        /* seconds */
+    long     tv_nsec;       /* nanoseconds */
+}ts;
 
 struct node{
-	signed long int TicketNo;
+	long long int TicketNo;
 	struct node *next;
-}*start, *newnode, *temp, *pre, *soldTickets;
+}*start, *newnode, *temp, *pre, *soldF, *soldR;
 
 ////Comment this if using TurboC
 void clrscr(){
@@ -13,7 +22,35 @@ void clrscr(){
 }
 ////
 
-void insertNewTicket(signed long int TicketNo){
+
+long randnum(long bound){
+    long r = 0;
+    int i;
+
+    for(i = 0; i < 10; i++){
+        clock_gettime(1, &ts);
+        srand((time_t)ts.tv_nsec);
+        r += rand();
+    }
+
+    return r % bound;
+}
+
+void insertIntoSellQueue(struct node *n){
+    if(soldF == NULL || soldR == NULL){
+        soldF = n;
+        soldR = n;
+        n->next = soldF;
+    }
+    else{
+        n->next = soldF;
+        soldR->next = n;
+        soldR = soldR->next;
+    }
+    soldTicketCounter++;
+}
+
+void insertNewTicket(long long int TicketNo){
     newnode = malloc(sizeof(struct node));
     newnode->TicketNo = TicketNo;
     newnode->next = NULL;
@@ -28,13 +65,29 @@ void insertNewTicket(signed long int TicketNo){
     }
 }
 
-void removeEL(){
+void trashTickets(){
+
+    while(start != NULL){
+        temp = start;
+        start = start->next;
+        temp->next = NULL;
+        free(temp);
+    }
+
+    if(soldR != NULL){
+        soldR->next = NULL;
+    }
+
+    while(soldF != NULL){
+        temp = soldF;
+        soldF = soldF->next;
+        temp->next = NULL;
+        free(temp);
+    }
+}
+
+void sellTicket(long loc){
     if(start != NULL){
-        show();
-
-        printf("Enter location of element to be deleted: ");
-        scanf("%d",&loc);
-
         if(loc > 1){
             count = 1;
 
@@ -45,47 +98,123 @@ void removeEL(){
                 temp = temp->next;
             }
             pre->next = temp->next;
-            free(temp);
 
         }
         else{
             temp = start;
             start = start->next;
-            free(temp);
         }
+
+        insertIntoSellQueue(temp);
+
     }
     else{
-        printf("Linklist Empty");
+        printf("Tickets not generated yet");
     }
 }
 
-void show(){
+void showAvailableTickets(){
     printf("-------------------\n");
     if(start != NULL){
         temp = start;
         count = 0;
 
+        printf("Sr no\tTicket Number\n");
         while(temp != NULL){
-            printf("%d\t: %d\n", ++count, temp->data);
+            printf("%li\t: %lld\n", ++count, temp->TicketNo);
             temp = temp->next;
         }
     }
     else{
-        printf("Linklist empty");
+        printf("Tickets Not Generated\n");
+    }
+    printf("-------------------\n");
+}
+
+void showSoldTickets(){
+    printf("-------------------\n");
+    if(soldF != NULL){
+        temp = soldF;
+        count = 0;
+
+        printf("Sr no\tTicket Number\n");
+        do{
+            printf("%li\t: %lld\n", ++count, temp->TicketNo);
+            temp = temp->next;
+        }while(temp != soldF);
+    }
+    else{
+        printf("No tickets Sold\n");
     }
     printf("-------------------\n");
 }
 
 void generateLotteryTickets(){
-    int LotteryTickets;
-    printf("Enter number of lottery tickets you want to generate");
+    int LotteryTickets, i;
+    long long int num, initial;
+	time_t now;
+    int hours, minutes, day, month, year;
+
+    trashTickets();
+    soldTicketCounter = 0;
+    printf("Enter number of lottery tickets : ");
     scanf("%d",&LotteryTickets);
 
+	time(&now);
+	struct tm *local = localtime(&now);
 
+    hours = local->tm_hour;
+    minutes = local->tm_min;
+    day = local->tm_mday;
+    month = local->tm_mon + 1;
+    year = (local->tm_year + 1900) % 100;
+
+    num = year;
+    num *= 100;
+    num += month;
+    num *= 100;
+    num += day;
+    num *= 100;
+    num += hours;
+    num *= 100;
+    num += minutes;
+    num *= 100000;
+    initial = num;
+
+    for(i = 0; i < LotteryTickets; i++){
+        insertNewTicket(num++);
+    }
+
+    printf("Lottery tickets generated from %lld to %lld.",initial, num);
 
 }
 
+void displayAndSell(){
+    long loc;
+
+    showAvailableTickets();
+
+    printf("Enter serial number to be selled: ");
+    scanf("%li",&loc);
+
+    sellTicket(loc);
+}
+
+long long int getWinner(){
+    long luckyNumber = randnum(soldTicketCounter);
+    count = -1;
+    temp = soldF;
+
+    while(count < luckyNumber){
+        temp = temp->next;
+        count++;
+    }
+
+    return temp->TicketNo;
+}
+
 int main(){
+    start = newnode = temp = pre = soldF = soldR = NULL;
 	while(1){
         clrscr();
 
@@ -94,22 +223,28 @@ int main(){
         printf("2. Display Aailable Tickets\n");
         printf("3. Display Sold Tickets\n");
         printf("4. Sell Ticket\n");
-        printf("5. Get Lottery Number\n");
+        printf("5. Get Winning Lottery Number\n");
         printf("6. Exit\n");
 
-        choice = getch();
+        choice = getch() - 48;
 
         switch(choice){
             case 1:
-                insert();
+                generateLotteryTickets();
                 break;
             case 2:
-                removeEL();
+                showAvailableTickets();
                 break;
             case 3:
-                show();
+                showSoldTickets();
                 break;
             case 4:
+                displayAndSell();
+                break;
+            case 5:
+                printf("The Lucky Winner is >>>%lld<<<",getWinner());
+                break;
+            case 6:
                 exit(0);
                 break;
             default:
---
+++ temp.c
@@ -0,0 +1,93 @@
+#include<stdio.h>
+#include<conio.h>
+#include<time.h>
+struct timespec {
+    time_t   tv_sec;        /* seconds */
+    long     tv_nsec;       /* nanoseconds */
+}ts;
+
+long randnum(long bound)
+{
+    long r = 0;
+    int i;
+
+    for(i = 0; i < 10; i++){
+        clock_gettime(1, &ts);
+        srand((time_t)ts.tv_nsec);
+        r += rand();
+    }
+
+    return r % bound;
+}
+
+void main(){
+    long long int a = 181023230300001;
+	time_t now;
+    int hours, minutes, seconds, day, month, year;
+
+    long x = 99998;
+
+    int i;
+ /*   printf("%lld\n\n", a);
+
+    a = a + 1;
+    printf("%lld\n\n", a);
+
+    a = a * 2;
+    printf("%lld\n\n", a);
+
+
+	time(&now);
+	struct tm *local = localtime(&now);
+
+    hours = local->tm_hour;      	// get hours since midnight (0-23)
+    minutes = local->tm_min;     	// get minutes passed after the hour (0-59)
+    seconds = local->tm_sec;     	// get seconds passed after minute (0-59)
+
+    day = local->tm_mday;        	// get day of month (1 to 31)
+    month = local->tm_mon + 1;   	// get month of year (0 to 11)
+    year = local->tm_year + 1900;	// get year since 1900
+
+
+	// print local time
+    printf("Time is : %02d:%02d:%02d am\n", hours, minutes, seconds);
+
+	// print current date
+	printf("Date is : %02d/%02d/%d\n\n\n", day, month, year);
+
+	a = year % 100;
+	a *=100;
+	a += month;
+	a = a*100;
+	a = a + day;
+	a = a*100;
+	a = a + hours;
+	a = a*100;
+	a = a + minutes;
+	a = a*100000;
+
+    printf("%lld\n\n", a++);
+    printf("%lld\n\n", a++);
+    printf("%lld\n\n", a++);
+    printf("%lld\n\n", a++);
+    printf("%lld\n\n", a++);
+
+    printf("0\t:a\n");
+    printf("00000\t:a\n\n\n");
+
+
+    printf("%li\n", x);
+    x++;
+    printf("%li\n", x);
+    x++;
+    printf("%li\n", x);
+    x++;
+*/
+    for(int i=0; i<40; i++){
+
+        //while(t < 25000){
+            printf("%d\n",randnum(123));
+       //}
+    }
+    getch();
+}
